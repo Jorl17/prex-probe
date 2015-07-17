13f49054d585e56b2c9b8505e78adeb619209eb6
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-07-17 20:54:20 +0100

    Adding Feature

---
+++ project/src/main/java/Feature.java
@@ -0,0 +1,25 @@
+/**
+ * Created by jorl17 on 17/07/15.
+ */
+public abstract class Feature {
+    private DataTimestamp timestamp;
+
+    public abstract String getRepresentation();
+
+    public Feature() {
+        this.timestamp = new DataTimestamp(); //Initialized to current time
+    }
+
+    public DataTimestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(DataTimestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + timestamp + ", " + getRepresentation() + ")";
+    }
+}
