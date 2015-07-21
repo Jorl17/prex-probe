4371c492e6cb22ee4d7ab58177609c404043db69
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-07-21 02:58:01 +0100

    Store file system list a priori to avoid run-time problems

--- project/src/main/java/DataSamplers/FileSystemSampler.java
+++ project/src/main/java/DataSamplers/FileSystemSampler.java
@@ -8,15 +8,25 @@ import org.hyperic.sigar.*;
  * Created by jorl17 on 18/07/15.
  */
 public class FileSystemSampler extends SimpleSamplerAdapter {
+    private FileSystem[] fsList;
     public FileSystemSampler(Sigar sigar) {
         super(sigar);
+
+        // Store FS List now so that it doesn't change at run-time (it would be painful to throw away data just because
+        // someone inserted a flash drive during our tests)
+        try {
+            fsList = sigar.getFileSystemList();
+        } catch (SigarException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void sampleData() {
         FileSystemUsage usage;
-        try {
-            for (FileSystem f : sigar.getFileSystemList()) {
+
+        for (FileSystem f : fsList) {
+            try {
                 usage = sigar.getFileSystemUsage(f.getDirName());
                 addFeature(new Data("FS # Reads (" + f.getDirName() + ")", "physical disk reads", usage.getDiskReads()));
                 addFeature(new Data("FS # Writes (" + f.getDirName() + ")", "physical disk writes", usage.getDiskWrites()));
@@ -26,11 +36,9 @@ public class FileSystemSampler extends SimpleSamplerAdapter {
                 addFeature(new Data("FS Free (" + f.getDirName() + ")", "KBytes", usage.getFree()));
                 addFeature(new Data("FS Total (" + f.getDirName() + ")", "KBytes", usage.getTotal()));
                 addFeature(new PercentValue("FS Used (" + f.getDirName() + ")", usage.getUsePercent()));
+            } catch (SigarException e) {
+                e.printStackTrace();
             }
-        } catch (SigarException e) {
-            e.printStackTrace();
         }
-
-
     }
 }
