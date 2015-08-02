a31fb468dd33dcfc42a2e6dfb1d9269561dafb56
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-08-02 04:09:16 +0100

    Use MiniPrexMonitor and JCommand for a working program

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -6,6 +6,8 @@ import DataLoggers.StdoutLogger;
 import DataSamplers.*;
 import Features.Feature;
 import Util.Utils;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import com.sun.org.apache.xpath.internal.operations.Mult;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
@@ -14,24 +16,30 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws SigarException {
-        Sigar sigar = new Sigar();
-        DataLogger logger = new StdoutAndCsvLogger("test.log");
-        Runtime.getRuntime().addShutdownHook(new Thread(logger::cleanup));
-        MultiFeatureSampler fullSampler = new MultiFeatureSampler(sigar, new CPUSampler(sigar), new MemorySampler(sigar), new FileSystemSampler(sigar), new TCPSampler(sigar), new NetIfaceSampler(sigar));
+    private static class Parameters {
+        @Parameter(names = {"-c", "--csv"}, description = "Output CSV file")
+        private String csv = "test.csv";
 
-        fullSampler.sample();
-        ArrayList<Feature> allFeatures = fullSampler.getProvidedFeatures();
-        logger.logHeader(new DataPoint(allFeatures));
-        for (int i = 0; i < 60000; i++) {
-            fullSampler.sample();
-            allFeatures = fullSampler.getProvidedFeatures();
-            logger.log(new DataPoint(allFeatures));
-            //for ( Feature f : allFeatures )
-                //System.out.print(f.getFeatureName() + ": " + f.getRepresentation() + " " + f.getUnits() + ", ");
-                //System.out.print(f.getRepresentation() + ", ");
-            //System.out.println();
-            Utils.sleepNoInterrupt(100);
+        @Parameter(names = {"-s", "--stdout"}, description = "Also log to stdout")
+        private boolean logStdout = true;
+
+        @Parameter(names = {"-p", "--period"}, description = "Sampling period")
+        private int samplingPeriod = 1000;
+
+        @Parameter(names = {"-h", "--help"}, help = true)
+        private boolean help;
+
+    }
+
+    public static void main(String[] args) throws SigarException {
+        Parameters parameters = new Parameters();
+        JCommander jcmd = new JCommander(parameters, args);
+        if (parameters.help) {
+            jcmd.usage();
+            return;
         }
+
+        MiniPrexMonitor miniPrexMonitor = new MiniPrexMonitor(parameters.csv, parameters.logStdout, parameters.samplingPeriod);
+        miniPrexMonitor.main();
     }
 }
