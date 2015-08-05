e3421035c7c94672f04725c4a309eb13273aaa2e
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-08-05 19:20:22 +0100

    Extend main() to support the new arguments. Now we can wait for programs and PIDs!

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -1,18 +1,13 @@
-import BuildingBlocks.DataPoint;
-import DataLoggers.CsvLogger;
-import DataLoggers.DataLogger;
-import DataLoggers.StdoutAndCsvLogger;
-import DataLoggers.StdoutLogger;
-import DataSamplers.*;
-import Features.Feature;
-import Util.Utils;
+import ExecutionManagers.CompositeExecutionManager;
+import ExecutionManagers.ExecutionManager;
+import ExecutionManagers.PidExecutionManager;
+import ExecutionManagers.ProcessLauncherManager;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
-import com.sun.org.apache.xpath.internal.operations.Mult;
-import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -26,9 +21,29 @@ public class Main {
         @Parameter(names = {"-p", "--period"}, description = "Sampling period")
         private int samplingPeriod = 1000;
 
+        @Parameter(names = {"-l", "--launch"}, description = "Launch application/command and wait until it ends")
+        private List<String> launchers = new ArrayList<>();
+
+        @Parameter(names = {"-pid", "--pid"}, description = "Wait until process with given PID has ended")
+        private List<Long> pids = new ArrayList<>();
+
         @Parameter(names = {"-h", "--help"}, help = true)
         private boolean help;
 
+    }
+
+    private static ArrayList<ExecutionManager> launchersToProcessLauncherArrayList(List<String> launchers) {
+        ArrayList<ExecutionManager> ret = new ArrayList<>();
+        for ( String l : launchers)
+            ret.add(new ProcessLauncherManager(l));
+        return ret;
+    }
+
+    private static ArrayList<ExecutionManager> pidToPidExecutionManagerArrayList(List<Long> pids) {
+        ArrayList<ExecutionManager> ret = new ArrayList<>();
+        for ( long p : pids)
+            ret.add(new PidExecutionManager(p));
+        return ret;
     }
 
     public static void main(String[] args) throws SigarException {
@@ -38,8 +53,13 @@ public class Main {
             jcmd.usage();
             return;
         }
+        ArrayList<ExecutionManager> executionManagers = new ArrayList<>();
+        executionManagers.addAll(launchersToProcessLauncherArrayList(parameters.launchers));
+        executionManagers.addAll(pidToPidExecutionManagerArrayList(parameters.pids));
 
+        CompositeExecutionManager manager = new CompositeExecutionManager(executionManagers);
         MiniPrexMonitor miniPrexMonitor = new MiniPrexMonitor(parameters.csv, parameters.logStdout, parameters.samplingPeriod);
-        miniPrexMonitor.main();
+
+        miniPrexMonitor.main(manager);
     }
 }
